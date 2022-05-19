fafed820c4c62fac3992725c72e43a4b6d55ac9a
Fundorin174 <fundorin174@gmail.com>
2022-05-19 17:20:24 +0300

    posts

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
 <div>
-    <p>Количество лайков <strong>{{likes}}</strong></p>
-    <p>Количество дизлайков <strong>{{dislikes}}</strong></p>
-</div>
-<div>
-    <button @click="addLike" >Like</button>
-    <button @click="addDislike">Dislike</button>
-</div>
+  <div class="post" v-for="post in posts" v-bind:key="post.id" >
+    <div><strong>Пост о</strong>{{' '+ post.title}}</div>
+    <div><strong>Описание</strong>{{post.body}}</div>
+  </div>
+  
+  </div>
 </template>
 
 <script>
 export default {
     data() {
         return {
-            likes: 0,
-            dislikes: 0,
+            posts: [
+              {id: 1, title: 'Javascript', body: 'Описание поста'},
+              {id: 2, title: 'Vue', body: 'Описание поста 2'},
+              {id: 3, title: 'React', body: 'Описание поста 3'},
+              {id: 4, title: 'Angular', body: 'Описание поста 4'},
+            ]
         }
     },
     methods: {
@@ -30,5 +33,15 @@ export default {
 </script>
 
 <style>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
+  .post {
+    padding: 15px;
+    border: 2px solid teal;
+    margin-bottom: 10px;
+  }
 </style>
