122ca16426f4fd7d2b019274999757415e50efdd
Fundorin174 <fundorin174@gmail.com>
2022-05-20 12:03:41 +0300

    add input

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
 <template>
 <div>
+  <form @submit.prevent>
+    <input 
+      @input='title=$event.target.value' 
+      v-bind:value="title" 
+      class="input" 
+      type="text" 
+      placeholder="Название" />
+    <input 
+      @input='description=$event.target.value'
+      v-bind:value="description" 
+      class="input" 
+      type='text' 
+      placeholder="Описание" />
+    <button class="btn" @click="addPost">Добавить</button>
+  </form>
   <div class="post" v-for="post in posts" v-bind:key="post.id" >
     <div><strong>Пост о</strong>{{' '+ post.title}}</div>
     <div><strong>Описание</strong>{{post.body}}</div>
@@ -17,15 +32,15 @@ export default {
               {id: 2, title: 'Vue', body: 'Описание поста 2'},
               {id: 3, title: 'React', body: 'Описание поста 3'},
               {id: 4, title: 'Angular', body: 'Описание поста 4'},
-            ]
+            ],
+            title: '',
+            description: '',
         }
     },
-    methods: {
-      addLike(){
-        this.likes +=1;
-      },
-      addDislike(){
-        this.dislikes +=1;
+    methods: {      
+      addPost(){
+        let postItem = {id: this.posts.length + Math.random(), title: this.title, body: this.description}
+        this.posts = [...this.posts, postItem]
       }
     }
 
@@ -39,9 +54,29 @@ export default {
     box-sizing: border-box;
   }
 
+  body {
+    padding: 20px;
+  }
+
   .post {
     padding: 15px;
     border: 2px solid teal;
     margin-bottom: 10px;
+    width: 400px;
+  }
+  form {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    width: 400px;
+  }
+  .input {
+    width: 100%;
+    border: 2px solid teal;
+    margin-bottom: 10px;
+  }
+  .btn {
+    margin: 20px 0;
+    align-self: flex-end;
   }
 </style>
